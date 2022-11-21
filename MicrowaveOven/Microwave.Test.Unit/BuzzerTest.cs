c5148fbdbf476b3a49e5a967b5c73a835cda173f
Marcin Szymanek <202009418@post.au.dk>
2022-11-21 19:40:52 +0100

    - Fixed Program.cs to correctly create AudioManager - Added AudioTests - Added tests of IAudio calls in BuzzerTest - Added tests of Buzzer functionality in UserInterfaceTest

--- MicrowaveOven/Microwave.Test.Unit/BuzzerTest.cs
+++ MicrowaveOven/Microwave.Test.Unit/BuzzerTest.cs
@@ -9,12 +9,13 @@ namespace Microwave.Test.Unit
     {
         private Buzzer _uut;
         private IOutput _output;
+        private IAudio _audio;
         [SetUp]
         public void Setup()
         {
             _output = Substitute.For<IOutput>();
-
-            _uut = new Buzzer(_output);
+            _audio = Substitute.For<IAudio>();
+            _uut = new Buzzer(_output, _audio);
         }
 
         [Test]
@@ -25,6 +26,13 @@ namespace Microwave.Test.Unit
         }
 
         [Test]
+        public void Toggle_WasOff_AudioReceived_Play()
+        {
+            _uut.Toggle();
+            _audio.Received(1).Play();
+        }
+
+        [Test]
         public void Toggle_WasOn_CorrectOutput()
         {
             _uut.Toggle();
@@ -32,6 +40,13 @@ namespace Microwave.Test.Unit
             _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("off")));
         }
 
+        [Test]
+        public void Toggle_WasOn_AudioReceived_Stop()
+        {
+            _uut.Toggle();
+            _uut.Toggle();
+            _audio.Received(1).Stop();
+        }
 
         [Test]
         public void Stop_CorrectOutput()
